9fb94b8f6ca1220c6ee72f64948e42e3e0c66fef
Remote Tester <remote.tester@example.com>
2026-03-10 12:58:02 +0800

    test: update remote pull verification script

--- HAIC/HAIC_V_1.1.4_R_1.1.3/database/pg/haic/004_DML_haic_remote_pull_case_V.sql
+++ HAIC/HAIC_V_1.1.4_R_1.1.3/database/pg/haic/004_DML_haic_remote_pull_case_V.sql
@@ -2,3 +2,7 @@
 INSERT INTO sys_config(config_key, config_value)
 VALUES ('haic.remote.pull.test', '2026-03-10')
 ON CONFLICT (config_key) DO UPDATE SET config_value = EXCLUDED.config_value;
+-- remote round 3
+UPDATE sys_config
+SET config_value = '2026-03-10-round3'
+WHERE config_key = 'haic.remote.pull.test';
